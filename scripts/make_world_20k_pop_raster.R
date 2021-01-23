37e33c134f121fd68cd309865f8ddb8c29d77d1e
eclee25 <eclee25@gmail.com>
2021-01-23 16:36:45 -0500

    aggregate world pop raster to 20km

---
+++ scripts/make_world_20k_pop_raster.R
@@ -0,0 +1,7 @@
+## aggregate to 20k for alignment
+
+## finally decided we needed 1x1km raster stack to ensure best alignment
+pop1k <- raster::stack("input_data/worldpop/ppp_2020_1km_Aggregated.tif")
+
+pop20k <- raster::aggregate(pop1k, 20, filename = "input_data/worldpop/ppp_2020_20km_Aggregated.tif")
+message("End script")
