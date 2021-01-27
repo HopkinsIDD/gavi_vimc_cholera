1dadb767e35920935bf7cd7389a62db2a4f86b98
eclee25 <eclee25@gmail.com>
2021-01-27 11:12:51 -0500

    put scenario configs in diff folders

--- scripts/write_configs.R
+++ scripts/write_configs.R
@@ -3,12 +3,21 @@
 
 source("scripts/set_all_parameters.R")
 
-pars <- tidyr::expand_grid(runname = runname, scenario = scenarios, country = countries, targeting = targeting_strategy, nsamples = num_samples, nskipyear = num_skip_years)
 cpathname <- file.path("configs", runname)
 dir.create(cpathname, showWarnings = FALSE)
 
-lapply(1:nrow(pars), function(i){
-  par_row <- pars[i,]
-  ocvImpact::prepare_config(par_row, cpathname)
-})
+for(scn in scenarios){
+
+  scnpathname <- file.path(cpathname, scn)
+  dir.create(scnpathname, showWarnings = FALSE)
+  pars <- tidyr::expand_grid(runname = runname, scenario = scn, country = countries, targeting = targeting_strategy, nsamples = num_samples, nskipyear = num_skip_years)
+
+  lapply(1:nrow(pars), function(i){
+    par_row <- pars[i,]
+    ocvImpact::prepare_config(par_row, scnpathname)
+  })
+
+}
+
+
 
