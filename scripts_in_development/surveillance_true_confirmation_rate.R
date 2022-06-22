2a53ba0ffc40f85bdcb0b9cfe6d716c297c317b7
KaiyueZou <kybryant@umich.edu>
2022-06-22 15:03:24 -0400

    deleted the old scripts

--- scripts_in_development/surveillance_true_confirmation_rate.R
+++
@@ -1,5 +0,0 @@
-surveillance_true_confirmation_rate <- function(datapath){
-  omicron_dataset <- readr::read_csv(paste0(datapath, "/confirmation_rate/parameters.csv"))
-  
-  return(omicron_dataset$mean)
-}
