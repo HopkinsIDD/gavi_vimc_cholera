4324fe6136911fc011a6f814f29f0436998ba46e
Christina Alam <calam1@jh.edu>
2024-02-29 12:54:04 -0500

    more debugging

--- packages/ocvImpact/R/utils_targeting.R
+++ packages/ocvImpact/R/utils_targeting.R
@@ -69,7 +69,10 @@ load_targets_by_country <- function(datapath, modelpath, country){
       bgd <- raster::raster(paste0(datapath, "/incidence/BGD_incid_5k_100.tif"))
       incid2 <- exactextractr::exact_extract(bgd, shp, 'mean') ## to use BGD incidence raster for targeting
     } else if (country %in% c("AFG", "HTI", "IRN", "IRQ", "NPL", "PAK", "PHL", "THA", "YEM", "IND")) { #for non-raster countries
-       file_path <- paste0(datapath, "/incidence/",country, "_incid_5k_", config$num_samples, ".tif")  
+       number_samples <- config$num_samples ##for debugging
+       print(number_samples) ##for debugging
+       file_path <- paste0(datapath, "/incidence/",country, "_incid_5k_", number_samples, ".tif")
+       rm(number_samples) ##for debugging
        message(paste0("Loading ", file_path))
        ##check that the incidence rate raster is cropped and exists 
        if (file.exists(file_path)){
