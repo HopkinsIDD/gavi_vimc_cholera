a17db8b6a69b9cb114db352ada69366124c525b2
Elizabeth Lee <eclee25@gmail.com>
2024-09-06 11:36:14 -0400

    rm unnecessary input data files and add a new one that represents the default countries

--- scripts/set_all_parameters.R
+++ scripts/set_all_parameters.R
@@ -21,12 +21,9 @@ if (runname == "202310gavi-4"){
 clean_outputs <- TRUE
 clean_incid <- FALSE
 
-# default country list
-ids <- readr::read_csv("input_data/locations_todeletelater.csv") # location ids
-cw <- readr::read_csv("input_data/region_country.csv")
-locs <- dplyr::right_join(cw, ids, by = c("country" = "region")) ## region & country grouping
-locs <- dplyr::filter(locs, !is.na(region))
-countries <- unique(locs$country)
+# load default country list
+countries <- readr::read_csv("input_data/default_modeled_countries.csv") %>%
+  dplyr::distinct(country) %>% unlist %>% unname
 # countries simulated in the VIMC core project
 countries <-c("AGO", "BDI", "BEN", "BFA", "CAF", "CIV", "CMR", "COD", "COG", "DZA", "ETH", "GHA", "GIN", "GNB", "KEN", "LBR", "MDG", "MLI",
               "MOZ", "MRT", "MWI", "NAM", "NER", "NGA", "RWA", "SEN", "SLE", "SOM", "SSD", "TCD", "TGO", "TZA", "UGA", "ZAF", "ZMB", "ZWE",
