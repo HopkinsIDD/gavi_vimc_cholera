5e5384464268c2bf052799f461a5f1cd19d8a89e
Christina Alam <calam1@jh.edu>
2024-09-11 13:01:41 +0300

    added data documentation R file for the OcvImpact package

---
+++ packages/ocvImpact/R/data_documentation.R
@@ -0,0 +1,14 @@
+#### This script adds documentation to package data ####
+
+#' surveillance_confirmation_rate_parameters
+#'
+#' Parameters for beta distribution that fits the confirmation rate distribution, Used for the surveillance project
+#' 
+#'
+#' @format ## `surveillance_confirmation_rate_parameters`
+#' A data frame with rows and columns:
+#' \describe{
+#'   \item{shape1}{parameter 1}
+#'   \item{shape2}{parameter 2}
+#'   ...
+#' }
