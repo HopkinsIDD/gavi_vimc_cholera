ec502244f15df6d31e738682eca159df44b7a565
Qulu <44678888+QLLZ@users.noreply.github.com>
2025-06-25 10:08:34 -0400

    Update utils_process_finaloutput.R

--- packages/ocvImpact/R/utils_process_finaloutput.R
+++ packages/ocvImpact/R/utils_process_finaloutput.R
@@ -35,18 +35,17 @@ adjust_deaths <- function(df, use_stochastic = F, multiplier = 1) {
 #' @title adjust_DALYs
 #' @description adjust the DALYs(YLLs) in the final model output file
 #' @param df the final model output file
-#' @param deaths_use_stochastic whether to adjust the deaths in the model output in a stochastic way
-#' @param cases_use_stochastic whether to adjust the cases in the model output in a stochastic way
+#' @param use_stochastic whether to adjust the model output in a stochastic way
 #' @param deaths_multiplier the single multiplier for deaths, the default is 1 (un-adjusted)
 #' @param cases_multiplier the single multiplier for cases, the default is 1 (un-adjusted)
 #' @return a dataframe with adjusted DALYs(YLLs) (same format as the final model output file)
 #' @export
-adjust_DALYs <- function(df, cases_use_stochastic = F, deaths_use_stochastic = F, cases_multiplier = 1, deaths_multiplier = 1) {
-  cases_multiplier <- if (!cases_use_stochastic) cases_multiplier else stop("stochastic scaling for cases is not implemented yet.")
-  deaths_multiplier <- if (!deaths_use_stochastic) deaths_multiplier else stop("stochastic scaling for deaths is not implemented yet.")
+adjust_DALYs <- function(df, use_stochastic = F, cases_multiplier = 1, deaths_multiplier = 1) {
+  cases_multiplier <- if (!use_stochastic) cases_multiplier else stop("stochastic scaling for cases is not implemented yet.")
+  deaths_multiplier <- if (!use_stochastic) deaths_multiplier else stop("stochastic scaling for deaths is not implemented yet.")
   
-  df$yll <- df$yll * deaths_multiplier
   df$yld <- (df$dalys-df$yll) * cases_multiplier
+  df$yll <- df$yll * deaths_multiplier
   df$dalys <- df$yld + df$yll
   
   return(df)
